10c7f4734f0a9662d0dc5c6b5fa5a58877ddb0c6
git-clay <ce.smith5525@gmail.com>
2017-01-05 19:39:31 -0700

    skeleton and dependancies

---
+++ server.js
@@ -0,0 +1,19 @@
+var exress	= require('express'),
+	app		= express();
+
+/*********************** Served up directories ******************************/
+app.use(express.static(__dirname + '/public'));
+app.set('views', '/public/views');
+
+
+app.get('/', function homepage(req, res){
+	res.sendFile(__dirname + '/views/index.html');
+});
+
+/*********************** ROUTES ******************************/
+
+
+/*********************** SERVER ******************************/
+app.listen(process.env.PORT || 3000, function() {
+	console.log('BOOM, Express is firing on all cylinders');
+});
