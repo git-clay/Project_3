2221dc6550d43970160591bd20731ca0a3e8b350
git-clay <ce.smith5525@gmail.com>
2017-01-16 09:36:10 -0700

    monday start

--- models/index.js
+++ models/index.js
@@ -1,75 +1,19 @@
-// 'use strict';
-
-// var fs        = require('fs');
-// var path      = require('path');
-// var Sequelize = require('sequelize');
-// var basename  = path.basename(module.filename);
-// var env       = process.env.NODE_ENV || 'development';
-// var config    = require(__dirname + '/../config/config')[env];
-// var db        = {};
-
-// //Create a Sequelize connection to the database using the URL in config/config.js
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(config.url, config);
-// }
-
-
-
-
-// //Load all the models
-// fs
-//   .readdirSync(__dirname)
-//   .filter(function(file) {	//automatically finds models js files
-//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-//   })
-//   .forEach(function(file) { // imports the db model name
-//     var model = sequelize['import'](path.join(__dirname, file));
-//     db[model.name] = model;
-//   });
-
-// Object.keys(db).forEach(function(modelName) {
-//   if (db[modelName].associate) { //gives the db keys
-//     db[modelName].associate(db);
-//   }
-// });
-
-// //Export the db Object
-// db.sequelize = sequelize;
-// db.Sequelize = Sequelize;
-
-// module.exports = db;
-
-/*    ^^es6 - might be best for heroku db^^   */
-
-
-
-// init will help start db
-// //import Sequelize from 'sequelize';
-// import init from 'sequelize-init'
- 
-// let sequelize = new Sequelize('database', 'username', 'password');
-// let db = init(sequelize, __dirname, {exclude: ['index.js']});
- 
-// export default db;
-
-
-
 /********** this must be on for heroku *************/
-// var pg = require('pg');
+var pg = require('pg');
+//Create a Sequelize connection to the database using the URL in config/config.js
+if (process.env.DATABASE_URL||process.env.ROAMRR_DB_URL) {
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL || process.env.ROAMRR_DB_URL, function(err,client){
+  if(err)throw err;
+  console.log('connected to postgres! Getting schemas....');
 
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL || process.env.ROAMRR_DB_URL, function(err,client){
-//   if(err)throw err;
-//   console.log('connected to postgres! Getting schemas....');
-
-//   client
-//   .query('Select table_schema,table_name FROM information_schema.tables;')
-//   .on('row',function(row){
-//     console.log(JSON.stringify(row));
-//   });
-// });
+  client
+  .query('Select table_schema,table_name FROM information_schema.tables;')
+  .on('row',function(row){
+    console.log(JSON.stringify(row));
+  });
+});
+}
 
 var thisComputer = process.env.LOGNAME;  //username to insert into new Sequelize
 var Sequelize = require('sequelize'),
@@ -107,3 +51,39 @@ module.exports.models = {
 	Event : Event,
 	Trip : Trip
 };
+
+// //Load all the models
+// fs
+//   .readdirSync(__dirname)
+//   .filter(function(file) {	//automatically finds models js files
+//     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+//   })
+//   .forEach(function(file) { // imports the db model name
+//     var model = sequelize['import'](path.join(__dirname, file));
+//     db[model.name] = model;
+//   });
+
+// Object.keys(db).forEach(function(modelName) {
+//   if (db[modelName].associate) { //gives the db keys
+//     db[modelName].associate(db);
+//   }
+// });
+
+// //Export the db Object
+// db.sequelize = sequelize;
+// db.Sequelize = Sequelize;
+
+// module.exports = db;
+
+/*    ^^es6 - might be best for heroku db^^   */
+
+
+
+// init will help start db
+// //import Sequelize from 'sequelize';
+// import init from 'sequelize-init'
+ 
+// let sequelize = new Sequelize('database', 'username', 'password');
+// let db = init(sequelize, __dirname, {exclude: ['index.js']});
+ 
+// export default db;
